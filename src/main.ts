a384a79bc27a544ce0a3d309e4ac9ab4d166fe4f
unknown <sumitmondal9448@gmail.com>
2024-12-23 16:19:58 +0530

    firstcommit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+let friends:string[];
+friends.push("Vamsy");
+friends.push("Sagar");
+friends.push("Suseela");
+
+console.log(friends);
+
+let nums:number[]=[1,2,3,4,5,6,7];
+console.log(nums);
+
+let weeks:readonly string[]=["sun","Mon","Tue","Wed","Thu","Fri","Sat"];
